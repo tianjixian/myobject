418d8f07432a2a30e58faae3cf31fdeebc7eb33b
tianjixian <188029741@qq.com>
2020-08-18 17:42:13 +0800

    更新swiper

--- src/views/home/swiper/mainswiper.vue
+++ src/views/home/swiper/mainswiper.vue
@@ -1,36 +1,44 @@
 <template>
-<div>
-    <swiper>
-        <swiper-slide><a>Slide 1</a></swiper-slide>
-        <swiper-slide><a>Slide 2</a></swiper-slide>
-        <swiper-slide><a>Slide 3</a></swiper-slide>
-        <swiper-slide><a>Slide 4</a></swiper-slide>
-        <swiper-slide><a>Slide 5</a></swiper-slide>
-        <div class="swiper-pagination" slot="pagination">左右按钮</div>
-    </swiper>
-</div>
-    
+  <swiper class="swiper" :options="swiperOption">
+    <swiper-slide>Slide 1</swiper-slide>
+    <swiper-slide>Slide 2</swiper-slide>
+    <swiper-slide>Slide 3</swiper-slide>
+    <swiper-slide>Slide 4</swiper-slide>
+    <swiper-slide>Slide 5</swiper-slide>
+    <div class="swiper-pagination" slot="pagination">
+    </div>
+  </swiper>
 </template>
 
 <script>
-// 导入swiper组件
-import swiper from '@/views/home/swiper/swiper.vue';
-import swiperSlide from '@/views/home/swiper/swiperSlide.vue';
-
-// import { Swiper, SwiperSlide, directive } from 'vue-awesome-swiper'
-// // If you use Swiper 6.0.0 or higher
-// import 'swiper/swiper-bundle.css'
+import { Swiper, SwiperSlide, directive } from 'vue-awesome-swiper'
+// import 'swiper/css/swiper.css'
+// If you use Swiper 6.0.0 or higher
+import 'swiper/swiper-bundle.css'
 
 export default {
-    name:'mainswiper',
-    components:{
-        swiper,
-        swiperSlide
+    name: 'mainswiper',
+    components: {
+        Swiper,
+        SwiperSlide
+    },
+    data() {
+      return {
+        swiperOption: {
+          pagination: {
+            el: '.swiper-pagination',
+          },
+          autoplay: {
+　　　　    delay: 3000,
+　　　　    disableOnInteraction: false // 手动切换之后继续自动轮播
+　　      },
+loop: true,
+        }
+      }
     }
-    
 }
 </script>
 
-<style>
+<style scoped>
 
 </style>
